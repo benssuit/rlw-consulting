ca3f57fd0ff13da027750037d67f6c11291dafec
Joff Monkton <jon.monkton@gmail.com>
2025-12-29 11:14:37 +0000

    register blocks

--- lib/acf-blocks.php
+++ lib/acf-blocks.php
@@ -2,7 +2,18 @@
 // ACF Blocks
 function register_acf_block_types()
 {
-	// 288
+	// 65 - Reviews
+	acf_register_block_type(array(
+		'name'              => 'block-65',
+		'title'             => __('Block - 65'),
+		'description'       => __('Codestitch 65'),
+		'render_template'   => 'blocks/65.php',
+		'category'          => 'layout',
+		'icon'              => 'list-view',
+		'keywords'          => array('Component', 'Codestitch', '65')
+	));
+
+	// 288 - Hero
 	acf_register_block_type(array(
 		'name'              => 'block-288',
 		'title'             => __('Block - 288'),
@@ -13,7 +24,40 @@ function register_acf_block_types()
 		'keywords'          => array('Component', 'Codestitch', '288')
 	));
 
-	// 2278
+	// 985 - Blog
+	acf_register_block_type(array(
+		'name'              => 'block-985',
+		'title'             => __('Block - 985'),
+		'description'       => __('Codestitch 985'),
+		'render_template'   => 'blocks/985.php',
+		'category'          => 'layout',
+		'icon'              => 'list-view',
+		'keywords'          => array('Component', 'Codestitch', '985')
+	));
+
+	// 1666 - Services
+	acf_register_block_type(array(
+		'name'              => 'block-1666',
+		'title'             => __('Block - 1666'),
+		'description'       => __('Codestitch 1666'),
+		'render_template'   => 'blocks/1666.php',
+		'category'          => 'layout',
+		'icon'              => 'list-view',
+		'keywords'          => array('Component', 'Codestitch', '1666')
+	));
+
+	// 1896 - Logos
+	acf_register_block_type(array(
+		'name'              => 'block-1896',
+		'title'             => __('Block - 1896'),
+		'description'       => __('Codestitch 1896'),
+		'render_template'   => 'blocks/1896.php',
+		'category'          => 'layout',
+		'icon'              => 'list-view',
+		'keywords'          => array('Component', 'Codestitch', '1896')
+	));
+
+	// 2278 - Side By Side
 	acf_register_block_type(array(
 		'name'              => 'block-2278',
 		'title'             => __('Block - 2278'),
